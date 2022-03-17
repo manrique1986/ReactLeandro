e0ae8745fb8a5be01e05594819ab75629ae7f0cf
Leandro Martini <leandromartini86@gmail.com>
2022-03-16 21:44:03 -0300

    ver

--- src/components/FinalizePurchase/FinalizePurchase.jsx
+++ src/components/FinalizePurchase/FinalizePurchase.jsx
@@ -9,9 +9,10 @@ const FinalizePurchase = () => {
 
 
         <div>
+             
 
             <h3 className="saludo">MUCHAS GRACIAS POR SU COMPRA !!</h3>
-
+        
             <img className="imgSaludo" src="https://res.cloudinary.com/dytpump6i/image/upload/v1641338521/halsey_olla_ycjta2.jpg" alt="" />
 
            
